490ac151d87224e0cd8223fff37b3ee44af86b9d
venkatesanraja <65437187+venkatesanraja@users.noreply.github.com>
2025-03-20 11:02:37 +0530

    Add files via upload

---
+++ R/data.R
@@ -0,0 +1,21 @@
+#' Example data set for construction of selection index
+#' @name data
+#' @docType data
+#' @usage data(data)
+#'
+#' A data set containing the information on random (sire), fixed (farm, season and period)
+#'  effects and three traits namely total milk yield, peak yield and age at first calving.
+#'  A total of 689 observations of daughters born to 7 sires are given.
+#'
+#' @format A data frame with 689 rows and 7 columns:
+#' \describe{
+#'   \item{sire}{sire of the cows}
+#'   \item{farm}{farm from which the data were collected}
+#'   \item{soc}{The season of calving of a cow}
+#'   \item{poc}{The period of calving of a cow}
+#'   \item{tmy}{Total lactation milk yield in Kg}
+#'   \item{py}{The peak yield in Kg}
+#'   \item{afc}{The age at first calving}
+#'   \item{fat}{The average fat percentage}
+
+NULL
